ac8588f118fc13c246373dedd64a10145a6a2358
Melina Escalante <mleiescalantee@gmail.com>
2024-08-30 10:36:51 -0300

    express

--- clase30-08/index.js
+++ clase30-08/index.js
@@ -1,4 +1,30 @@
-const chalk = require('chalk')
+const chalk = require("chalk");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log(chalk.blueBright('Soy el azul'))
-console.log(chalk.magenta('Color magenta'))
+//Ruta Home
+
+app.get("/", (response, request) => {
+  response.send("Home");
+  console.log(chalk.cyan("Cliente ingresó al home"));
+});
+
+//Ruta Productos
+
+app.get("/products", (response, request) => {
+  response.send("Productos");
+  console.log(chalk.cyan("Cliente ingresó a la sección de productos"));
+});
+//Ruta con parametros
+
+app.get("/products/:id", (request, response ) => {
+  const id = request.params.id;
+  response.send(`Producto ${id}`);
+  console.log(chalk.cyan(`Cliente ingresó al producto: ${id}`));
+});
+
+
+app.listen(port, () => {
+  console.log(chalk.yellow(`Servidor escuchando en el puerto 3000 a la app`));
+});
